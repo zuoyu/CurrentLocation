039e4e58b2a8d17e58fba26d3409c0e8c7aeecab
Yu Zuo <zuo2@cs.ualberta.ca>
2014-02-04 19:14:27 -0700

    did

--- src/ca/ualberta/cs/comput301/currentlocation/CurrentLocationActivity.java
+++ src/ca/ualberta/cs/comput301/currentlocation/CurrentLocationActivity.java
@@ -1,5 +1,7 @@
 package ca.ualberta.cs.comput301.currentlocation;
 
+import java.util.Date;
+
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -17,15 +19,47 @@ public class CurrentLocationActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		
+		
+		
+		
 		// Obtain LocationManager service and set up location update request.
 		// To Do
+		
+		LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
 	}
 
 	
 	// Retrieve location updates through LocationListener interface
 	private final LocationListener locationListener = new LocationListener(){
 	// To Do: override the four methods.
-	}	
+		public void onLocationChanged (Location location) {
+			TextView tv = (TextView) findViewById(R.id.myLocationText);
+			if (location != null) {
+				double lat = location.getLatitude();
+				double lng = location.getLongitude();
+				Date date = new Date(location.getTime());
+				tv.setText("Your location is:"
+						+ "\nLatitude: " + lat
+						+ "\nLongtitude: " + lng
+						+ "\nat: " + date.toString());
+			} else {
+				tv.setText("No location infomation");
+			}
+		}
+		
+		public void onProviderDisabled (String provider) {
+			
+		}
+		
+		public void onProviderEnabled (String provider) {
+			
+		}
+		
+		public void onStatusChanged (String provider, int status, Bundle extras) {
+			
+		}	
+	};
 
 	
 	@Override
